76e5f337c9d0093c4b7d48d571a01cad87aa6f48
rizvandwiki <rizvand21@gmail.com>
2022-04-26 17:21:46 +0700

    update package setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    packages=["dumboo"],
+    packages=setuptools.find_packages(),
     long_description=long_description,
     install_requires= [
         "setuptools>=40",
