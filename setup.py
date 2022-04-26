2984ebfc5e7ff5af7c2c6adb0ca8c42f6c2342a0
rizvandwiki <rizvand21@gmail.com>
2022-04-26 17:20:37 +0700

    update structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    packages=setuptools.find_packages(include=['dumbo', 'dumbo.*']),
+    packages=["dumboo"],
     long_description=long_description,
     install_requires= [
         "setuptools>=40",
