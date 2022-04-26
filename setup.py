d061d143bef1e27298c2c9e0218788e61ce144b6
rizvandwiki <rizvand21@gmail.com>
2022-04-26 16:28:39 +0700

    edit required dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-package_dir={"": "dumbo"},
-packages=setuptools.find_packages(where="dumbo"),
-long_description=long_description
+    package_dir={"": "dumbo"},
+    packages=setuptools.find_packages(where="dumbo"),
+    long_description=long_description,
+    install_requires= [
+        "setuptools>=40",
+        "wheel",
+        "Faker>=13.4.0"
+    ]
 )
