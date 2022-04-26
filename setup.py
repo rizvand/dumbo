f6de5db181557d4e40d7658a9a7f0cd8a8fc5431
rizvandwiki <rizvand21@gmail.com>
2022-04-26 16:58:28 +0700

    update package setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
  
 setuptools.setup(
     package_dir={"": "dumbo"},
-    packages=setuptools.find_packages(where="dumbo"),
+    packages=["dumbo"],
     long_description=long_description,
     install_requires= [
         "setuptools>=40",
